9ff658b9a71724d21d063d126e2269c12a7a401b
sameemanvar <111353411+sameemanvar@users.noreply.github.com>
2022-09-11 10:37:04 +0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,67 +1,48 @@
-# Download the unzipped file first and save the zipped file to local document
+
 # library to be used:
 library(dplyr)
-## Next get:
+
 # get train data
 xtrain<-read.table('./UCI HAR Dataset/train/X_train.txt', header=FALSE)
 ytrain<-read.table('./UCI HAR Dataset/train/y_train.txt', header=FALSE)
+subtrain<-<-read.table('./UCI HAR Dataset/train/subject_train.txt', header=FALSE)
+
 # get test data
 xtest<-read.table('./UCI HAR Dataset/test/X_test.txt', header=FALSE)
 ytest<-read.table('./UCI HAR Dataset/test/y_test.txt', header=FALSE)
-# get features data
+subtrain<-<-read.table('./UCI HAR Dataset/train/subject_test.txt', header=FALSE)
+
+# read features description
 features<-read.table('./UCI HAR Dataset/features.txt', header=FALSE)
-# get activity data
+
+# read activity labels
 activity<-read.table('./UCI HAR Dataset/activity_labels.txt', header=FALSE)
-# get subject data
-subtrain<-read.table('./UCI HAR Dataset/train/subject_train.txt', header=FALSE)
-subtrain<-subtrain%>%
-  rename(subjectID=V1)
-subtest<-read.table('./UCI HAR Dataset/test/subject_test.txt', header=FALSE)
-subtest<-subtest%>%
-  rename(subjectID=V1)
 
-# add column names to both train and test data
-features<-features[,2]
-featrasp<-t(features)
-colnames(xtrain)<-featrasp
-colnames(xtest)<-featrasp
-# rename activity columns to id and actions(walk,lay,etc.)
-colnames(activity)<-c('id','actions')
+#merge of training and test sets
+x_total<-rbind(x_train, x_test)
+y_total<-rbind(y_train, y_test)
+sub_total<-rbind(sub_train, sub_test)
 
-# row bind xtrain and xtest 
-combineX<-rbind(xtrain, xtest)
-# row bind ytrain and ytest
-combineY<-rbind(ytrain, ytest)
-# row bind subject train and subject test
-combineSubj<-rbind(subtrain,subtest)
+# to keep mean and SD
+total_features<-var_names('.*mean\\(\\)|std\\(\\)", features[,2], ignore.case = FALSE),]
+x_total<-x_total[,total_features[,1]]
 
-# column bind Y and X (the two data frames created above). We then have everything except for activity 
-YXdf<-cbind(combineY,combineX, combineSubj)
+#to name columns
+colnames(x_total)<-sel_features[,2]
+colnames(y_total)<-"activity"
+colnames(sub_total)<-"subject"
 
-# merge the above data frame with the activity
-df<-merge(YXdf, activity,by.x = 'V1',by.y = 'id')
+# merge final dataset
+total<-cbind(sub_total, y_total, x_total)
 
+# turn activities & subjects into factors 
+total$activity<-factor(total$activity, levels = activity_labels[,1], labels = activity_labels[,2]) 
+total$subject<-as.factor(total$subject) 
 
+# create a summary independent tidy dataset from final dataset 
+# with the average of each variable for each activity and each subject. 
 
+total_mean<-total %>% group_by(activity, subject) %>% summarize_all(funs(mean)) 
 
-# getting the mean and standard deviation
-colNames<-colnames(df)
-df2<-df%>%
-  select(actions, subjectID, grep("\\bmean\\b|\\bstd\\b",colNames))
-
-# transform activity to a factor variable 
-df2$actions<-as.factor(df2$actions)
-
-# use descriptive activity names to name the activities in the data set
-colnames(df2)<-gsub("^t", "time", colnames(df2))
-colnames(df2)<-gsub("^f", "frequency", colnames(df2))
-colnames(df2)<-gsub("Acc", "Accelerometer", colnames(df2))
-colnames(df2)<-gsub("Gyro", "Gyroscope", colnames(df2))
-colnames(df2)<-gsub("Mag", "Magnitude", colnames(df2))
-colnames(df2)<-gsub("BodyBody", "Body", colnames(df2))
-
-# creates a second data set with the average of each variable for activity and subject.
-df2.2<-aggregate(. ~subjectID + actions, df2, mean)
-
-# text file for final output
-write.table(df2.2, file = "tidydata.txt",row.name=FALSE)
+# export summary dataset
+write.table(total_mean, file = "./tidydataset.txt", row.names = FALSE, col.names = TRUE) 
